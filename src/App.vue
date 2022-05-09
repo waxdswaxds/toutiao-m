3c24ec1453b730d5a835d54a96278f4316b856b1
liangmingyan <1947359424@qq.com>
2022-05-09 11:10:02 +0800

    项目初始化-调整初始目录结构

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,15 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <nav></nav>
+    <router-view />
   </div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+export default {
+  name: 'App', // 可以在调试工作找到这个组件,组件name
+  data() {
+    return {}
   }
 }
-</style>
+</script>
+<style lang="less"></style>
